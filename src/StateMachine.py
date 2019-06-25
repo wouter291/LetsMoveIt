722415e6790107c93fb9a84e905c71fe227c6d04
wouter291 <52036531+wouter291@users.noreply.github.com>
2019-06-25 14:53:36 +0200

    Update StateMachine.py

--- src/StateMachine.py
+++ src/StateMachine.py
@@ -6,7 +6,7 @@ import yaml
 from geometry_msgs.msg import Pose, TwistStamped
 from std_msgs.msg import String
 from SendMove import SendMove
-from io_interface import set_digital_out, get_states, set_states, callback
+from io_interface import set_digital_out, set_states, callback
 from ur_msgs.srv import *
 from ur_msgs.msg import *
 
@@ -16,22 +16,20 @@ class StateMachine (object):
         rospy.init_node('StateMachineNode', anonymous=True)
         self.pub = rospy.Publisher('/ur_driver/URScript', String, queue_size=10, latch=True)
     
-        self.addr = '/home/ubuntu/catkin_ws/src/LetsMoveIt/yaml/poses.yaml'
+        self.addr = '/home/ubuntu/catkin_ws/src/ur_script_control/yaml/poses.yaml'
         rospy.Subscriber("tool_velocity", TwistStamped, self.getToolVelocity)
 
         set_states()
         rospy.Subscriber("/ur_driver/io_states", IOStates, self.callback)     
         self.Digital_Out_States = [0,0,0,0,0,0,0,0,0,0]  #8(controller)+2(tool)
         self.Digital_In_States = [0,0,0,0,0,0,0,0,0,0]   #8(controller)+2(tool)
-        self.Analog_Out_States = [0,0]  #2(controller)
-        self.Analog_In_States = [0,0]   #2(controller)+0(tool)
-        #get_states()
         
         #Set output to default
         set_digital_out(0,1) #opening gripper
         set_digital_out(1,0) #close gripper
+        set_digital_out(7,0) #Ready signal false
 
-       
+
     def getToolVelocity(self, msg):
         self.xVel = msg.twist.linear.x
         self.yVel = msg.twist.linear.y
@@ -40,7 +38,6 @@ class StateMachine (object):
         self.ryVel = msg.twist.angular.y
         self.rzVel = msg.twist.angular.z
 
-   
     def callback(self, data):
         for i in range(0,10):
             del self.Digital_Out_States[i]
@@ -67,33 +64,23 @@ class StateMachine (object):
         #print wanted_pose
         return wanted_pose
 
-    
     def buildMove(self, moveType, pose):
-        # acceleration = 1  #Joint acceleration in rad/s^2
-        # speed = 0.8 #Joint speed in rad/s
         time = 0 #Time the move must take
         radius = 0 # Blend radius in m, so the robot moves trough the point instead of stopping
         array = list(pose)
-        #print array
         sendable = "move%s(%s, a=%s, v=%s, t=%s, r=%s)" % (moveType, array, self.acceleration, self.speed, time, radius)
-        #sendable = "movej(p[0.00, 0.3, 0.4, 2.22, -2.22, 0.00], a=1.0, v=0.1"
-        #print sendable
         return sendable
 
-    
     def sendMove(self, string):
         st = String()        
         st.data = string
         self.pub.publish(st)
 
-        
     def testSequence(self):
         try:
-            # #Starting the robot
-            # print("Press 'Enter' to start the robot")
-            # raw_input()
             excecute = StateMachine() 
 
+            #Get all inputs in local variable
             input0 = self.Digital_In_States[0]
             input1 = self.Digital_In_States[1]
             input2 = self.Digital_In_States[2]
@@ -102,20 +89,19 @@ class StateMachine (object):
             input5 = self.Digital_In_States[5]
             input6 = self.Digital_In_States[6]
             input7 = self.Digital_In_States[7]
-            #print "input4 =", input4
-            #time.sleep(1)
+            print "input4 =", input4
+
+            #Move to start position
+            self.speed = 0.8
+            self.acceleration = 0.8
+            self.sendMove(self.buildMove('j',self.getPos('Abovelift')))
+            excecute.waitForPosition()
 
 
-            if (input4 == 0):
+            if (input4 == 1):
 
                 #ready signal for lift to false
                 set_digital_out(7,0) 
-
-                #Abovelift
-                self.speed = 0.8
-                self.acceleration = 0.8
-                self.sendMove(self.buildMove('j',self.getPos('Abovelift')))
-                excecute.waitForPosition()
                 
                 #PickingBoxAtLift
                 self.speed = 0.8
@@ -181,7 +167,6 @@ class StateMachine (object):
                 self.sendMove(self.buildMove('j',self.getPos('Abovelift')))
                 excecute.waitForPosition()
 
-
         except rospy.ROSInternalException:
             return
         except KeyboardInterrupt:
@@ -193,4 +178,3 @@ if __name__ == '__main__':
         objStateMachine = StateMachine()
         objStateMachine.testSequence()
        
-
